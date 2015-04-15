65ba50e5a799be4ab14d3608eccd3818b6655e6d
focinfi <focinfi@gmail.com>
2015-04-15 12:46:54 +0800

    change the right file

--- RubySyntax/ruby_inject.rb
+++ RubySyntax/ruby_inject.rb
@@ -1,24 +1,38 @@
 # Implement ruby Array inject func as my_inject
-# 1. 破坏原数组
-# 2. meth = meth || blk
 module Enumerable
-  def my_inject(acc=nil, meth=nil, &blk)
-    meth,acc = [acc,nil] unless blk || meth
-    meth = meth || blk
-    raise "should provide method_name or block" unless meth && meth.respond_to?(:to_proc)
+  def my_inject *arg
+    initial = method = nil 
+    
+    tmp = Array.new(self)
 
-    dup.arr_inject(acc, &meth.to_proc)
-  end
-  def arr_inject(acc)
-    acc ||= shift
-    each {|v| acc = yield acc,v}
-    acc
+    if arg.length > 2
+      raise ArgumentError, "wrong number of arguments #{arg.length} for 0..2"
+    elsif arg.length == 2
+      initial, method = arg[0], arg[1] 
+    elsif arg.first.is_a? Symbol
+      method = arg.first
+    elsif arg.first
+      initial = arg.first
+    end
+
+    accumulation = initial ? initial : tmp.shift
+  
+    if method 
+      tmp.each do |element|
+        accumulation = accumulation.send(method, element)
+      end
+    else 
+      tmp.each do |element|
+        accumulation = yield accumulation, element
+      end
+    end
+		
+    accumulation
   end
 end
-
-
+# p [1, 2, 3].my_inject(:*, &:*){}
 require 'minitest/autorun'
-class TestMyInject < MiniTest::Unit::TestCase
+class TestMyInject < Minitest::Test
 	def setup
 		@num_arr = [1, 2, 3, 4]
 		@sum_proc = Proc.new { |s, i| s + i * 10 }
@@ -26,8 +40,8 @@ class TestMyInject < MiniTest::Unit::TestCase
 	end
 
 	def test_sum_elements_with_initial_num
-		assert_equal @num_arr.inject(100, &@sum_proc), 
-			@num_arr.my_inject(100, &@sum_proc)
+		assert_equal @num_arr.inject(100.2, &@sum_proc), 
+			@num_arr.my_inject(100.2, &@sum_proc)
 	end
 
 	def test_sum_elements_without_initial_num
@@ -36,8 +50,8 @@ class TestMyInject < MiniTest::Unit::TestCase
 	end
 
 	def test_multi_elements_with_initial_num
-		assert_equal @num_arr.inject(100, &@multi_proc), 
-			@num_arr.my_inject(100, &@multi_proc)
+		assert_equal @num_arr.inject(100.2, &@multi_proc), 
+			@num_arr.my_inject(100.2, &@multi_proc)
 	end
 
 	def test_multi_elements_with_initial_num
@@ -52,9 +66,13 @@ class TestMyInject < MiniTest::Unit::TestCase
 	def test_multi_elements_with_multi_symbol
 		assert_equal @num_arr.inject(:*), @num_arr.my_inject(:*)
 	end
+	
+	def test_two_params_with_initial_sum_symbol
+		assert_equal @num_arr.inject(10.2, :+), @num_arr.my_inject(10.2, :+)
+	end
 
-	def test_pass_a_block_param
-		assert_equal @num_arr.inject(1, :+, &:*), @num_arr.my_inject(1, :+, &:*)
+	def test_with_block_parameter
+		assert_equal @num_arr.inject(&:+), @num_arr.my_inject(&:+)
 	end
 
 	def test_pass_a_block_param
@@ -65,6 +83,12 @@ class TestMyInject < MiniTest::Unit::TestCase
 		rang = (1..10)
 		assert_equal @num_arr.inject(:+), @num_arr.my_inject(:+)
 	end
+
 end
 
-#
+
+
+
+
+
+ 
